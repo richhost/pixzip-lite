f57bad2ecc8837ce62c056e421223e262983d032
Abiee <usckur@gmail.com>
2023-01-28 18:40:25 +0800

    adapt Windows

--- src/layout/Aside.tsx
+++ src/layout/Aside.tsx
@@ -5,10 +5,22 @@ type Props = {
   children?: React.ReactNode;
 };
 
+const isMacOS = window.lossApi["isMacOS"];
+
 const Aside: React.FC<Props> = ({ children }) => {
   return (
     <aside className="aside">
-      <div className="drag">Logo</div>
+      <div className="drag">
+        {!isMacOS && (
+          <div className="brand">
+            <img
+              srcSet="/icons/win/logo.png 1x, /icons/win/logo@2x.png 2x, /icons/win/logo@3x.png 3x"
+              alt="logo"
+            />
+            像素丢失
+          </div>
+        )}
+      </div>
       <div className="aside-body">{children}</div>
     </aside>
   );
