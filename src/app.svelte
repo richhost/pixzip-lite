74a13924ede23a8c622f064415f5b47006b63b4f
richhost <hexcy@pm.me>
2024-01-04 15:29:19 +0800

    ui: scrollbar

--- src/app.svelte
+++ src/app.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import Brand from './lib/components/brand.svelte';
 	import Header from './lib/components/header.svelte';
+	import Scrollbar from './lib/components/scrollbar.svelte';
+	import Workspace from './lib/components/workspace.svelte';
 </script>
 
 <div id="layout">
 	<aside class="aside">
 		<Brand />
+		<Scrollbar>
+			<Workspace />
+		</Scrollbar>
 	</aside>
 	<main class="main">
 		<Header />
@@ -22,6 +27,8 @@
 		background-color: var(--gray-3);
 		width: clamp(180px, 20%, 240px);
 		border-inline-end: 1px solid var(--gray-5);
+		display: flex;
+		flex-direction: column;
 	}
 
 	.main {
---
+++ src/lib/components/scrollbar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import SimpleBar from 'simplebar';
+	import 'simplebar/dist/simplebar.css';
+
+	let { children } = $props<{ children: any }>();
+
+	let scrollWrapper: HTMLDivElement;
+
+	$effect(() => {
+		const simpleBar = new SimpleBar(scrollWrapper, {
+			autoHide: true
+		});
+
+		return () => simpleBar.unMount();
+	});
+</script>
+
+<div class="scroll-wrapper" bind:this={scrollWrapper}>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style>
+	.scroll-wrapper {
+		height: 100%;
+		overflow: auto;
+	}
+</style>
