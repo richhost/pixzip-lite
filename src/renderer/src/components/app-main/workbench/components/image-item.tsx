2bc52a743c0e3723581e1b58b39e65ef18b2c62c
richhost <hexcy@pm.me>
2024-01-17 15:07:16 +0800

    ui: image item style

--- src/renderer/src/components/app-main/workbench/components/image-item.tsx
+++ src/renderer/src/components/app-main/workbench/components/image-item.tsx
@@ -2,6 +2,7 @@ import {
 	ExternalLinkIcon,
 	ArrowDownIcon,
 	CopyIcon,
+	Cross1Icon,
 } from "@radix-ui/react-icons";
 import * as Progress from "@radix-ui/react-progress";
 
@@ -13,13 +14,13 @@ export function ImageItem() {
 		<div className="relative rounded-lg overflow-hidden border">
 			<Progress.Root className="absolute inset-0 rounded-lg overflow-hidden">
 				<Progress.Indicator
-					className="w-full h-full bg-secondary opacity-50 absolute z-10"
+					className="w-full h-full bg-secondary opacity-30 absolute z-10"
 					style={{
 						transform: "translateX(0%)",
 					}}
 				/>
 			</Progress.Root>
-			<div className="relative z-10 text-sm p-3 flex flex-wrap xl:flex-nowrap items-center justify-between gap-x-5 gap-y-1">
+			<div className="relative z-10 text-sm p-3 flex flex-wrap xl:flex-nowrap items-center justify-between gap-x-5 gap-y-2">
 				<div className="flex items-center gap-2 grow">
 					<p className="font-medium truncate">zhanfangfweaf.jpg</p>
 					<p className="flex-shrink-0 text-muted-foreground">5 MB</p>
@@ -31,12 +32,17 @@ export function ImageItem() {
 						<ArrowDownIcon />
 						<span>22%</span>
 					</Badge>
-					<Button variant="ghost" className="w-6 h-6 px-0">
-						<CopyIcon className="pointer-events-none" />
-					</Button>
-					<Button variant="ghost" className="w-6 h-6 px-0">
-						<ExternalLinkIcon className="pointer-events-none" />
-					</Button>
+					<div className="flex items-center gap-1">
+						<Button variant="ghost" className="w-6 h-6 px-0">
+							<Cross1Icon className="pointer-events-none" />
+						</Button>
+						<Button variant="ghost" className="w-6 h-6 px-0">
+							<CopyIcon className="pointer-events-none" />
+						</Button>
+						<Button variant="ghost" className="w-6 h-6 px-0">
+							<ExternalLinkIcon className="pointer-events-none" />
+						</Button>
+					</div>
 				</div>
 			</div>
 		</div>
