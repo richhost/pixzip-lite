da555172d441209d9fa076e9c06b799c5092ec0f
richhost <hexcy@pm.me>
2024-04-21 19:18:37 +0800

    Revert "remove electron-clipboard-ex"
    This reverts commit fb40aeee6fb0e1faa2acf4887b0f976610981615.

--- src/main/ipc/handlers.ts
+++ src/main/ipc/handlers.ts
@@ -1,4 +1,5 @@
 import { dialog, ipcMain, shell } from "electron";
+// import clipboardEx from "electron-clipboard-ex";
 import { clipboard } from "clipboard-sys";
 
 export const registerHandlers = async () => {
@@ -9,6 +10,7 @@ export const registerHandlers = async () => {
     return filePaths;
   });
   ipcMain.on("copy", async (_, filepath: string) => {
+    // clipboardEx.writeFilePaths([filepath]);
     clipboard.writeFiles(filepath);
   });
   ipcMain.on("trash", async (_, filepath: string) => {
