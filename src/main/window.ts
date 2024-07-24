80442d1207a65a5594bfc7c3ac7fbb27d80c439c
richhost <hexcy@pm.me>
2024-07-25 00:04:43 +0800

    ui: trafficLightPosition

--- src/main/window.ts
+++ src/main/window.ts
@@ -1,70 +1,68 @@
-import { fileURLToPath } from "node:url";
-import { BrowserWindow, app } from "electron";
-import windowStateKeeper from "electron-window-state";
+import { fileURLToPath } from 'node:url';
+import { BrowserWindow, app } from 'electron';
+import windowStateKeeper from 'electron-window-state';
 
 const platform = process.platform;
 
 async function createWindow() {
-  const mainWindowState = windowStateKeeper({
-    defaultWidth: 800,
-    defaultHeight: 500,
-  });
+	const mainWindowState = windowStateKeeper({
+		defaultWidth: 800,
+		defaultHeight: 500
+	});
 
-  const browserWindow = new BrowserWindow({
-    x: mainWindowState.x,
-    y: mainWindowState.y,
-    width: mainWindowState.width,
-    height: mainWindowState.height,
-    minWidth: 800,
-    minHeight: 400,
-    frame: !(platform === "linux"),
-    titleBarStyle: "hidden",
-    trafficLightPosition: { x: 10, y: 14 },
-    vibrancy: "sidebar",
-    visualEffectState: "active",
-    transparent: platform === "darwin",
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      sandbox: false,
-      preload: fileURLToPath(new URL("../preload/index.mjs", import.meta.url)),
-    },
-  });
+	const browserWindow = new BrowserWindow({
+		x: mainWindowState.x,
+		y: mainWindowState.y,
+		width: mainWindowState.width,
+		height: mainWindowState.height,
+		minWidth: 800,
+		minHeight: 400,
+		frame: !(platform === 'linux'),
+		titleBarStyle: 'hidden',
+		trafficLightPosition: { x: 10, y: 8 },
+		// vibrancy: "sidebar",
+		visualEffectState: 'active',
+		transparent: platform === 'darwin',
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true,
+			sandbox: false,
+			preload: fileURLToPath(new URL('../preload/index.mjs', import.meta.url))
+		}
+	});
 
-  if (!app.isPackaged && process.env.ELECTRON_RENDERER_URL) {
-    browserWindow.loadURL(process.env.ELECTRON_RENDERER_URL);
-    browserWindow.webContents.openDevTools();
-  } else {
-    browserWindow.loadFile(
-      fileURLToPath(new URL("../renderer/index.html", import.meta.url))
-    );
-  }
+	if (!app.isPackaged && process.env.ELECTRON_RENDERER_URL) {
+		browserWindow.loadURL(process.env.ELECTRON_RENDERER_URL);
+		browserWindow.webContents.openDevTools();
+	} else {
+		browserWindow.loadFile(fileURLToPath(new URL('../renderer/index.html', import.meta.url)));
+	}
 
-  mainWindowState.manage(browserWindow);
-  return browserWindow;
+	mainWindowState.manage(browserWindow);
+	return browserWindow;
 }
 
 let browserWindow: BrowserWindow | undefined;
 
 export async function restoreOrCreateWindow() {
-  const gotTheLock = app.requestSingleInstanceLock();
-  if (!gotTheLock) app.quit();
-  browserWindow = BrowserWindow.getAllWindows().find((w) => !w.isDestroyed());
+	const gotTheLock = app.requestSingleInstanceLock();
+	if (!gotTheLock) app.quit();
+	browserWindow = BrowserWindow.getAllWindows().find((w) => !w.isDestroyed());
 
-  if (browserWindow === undefined) {
-    browserWindow = await createWindow();
-  }
+	if (browserWindow === undefined) {
+		browserWindow = await createWindow();
+	}
 
-  if (browserWindow.isMinimizable()) {
-    browserWindow.restore();
-  }
+	if (browserWindow.isMinimizable()) {
+		browserWindow.restore();
+	}
 
-  return browserWindow;
+	return browserWindow;
 }
 
 export async function getMainWindow() {
-  if (browserWindow === undefined) {
-    return await restoreOrCreateWindow();
-  }
-  return browserWindow;
+	if (browserWindow === undefined) {
+		return await restoreOrCreateWindow();
+	}
+	return browserWindow;
 }
--- src/renderer/src/lib/components/title-bar/title-bar.svelte
+++ src/renderer/src/lib/components/title-bar/title-bar.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import WindowCtr from "./window-ctr.svelte";
+	import WindowCtr from './window-ctr.svelte';
 </script>
 
 <div class="draggable flex h-[var(--h-title-bar)]">
-  <div class="w-[clamp(var(--w-sidebar-min),16vw,var(--w-sidebar-max))]"></div>
-  <div class="relative grow">
-    <WindowCtr />
-  </div>
+	<div class="w-[clamp(var(--w-sidebar-min),16vw,var(--w-sidebar-max))]"></div>
+	<div class="relative grow">
+		<WindowCtr />
+	</div>
 </div>
